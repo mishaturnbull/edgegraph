49a6822ffe50e6193d1ce18217f0aaa7731a63b4
Misha Turnbull <mishaturnbull@gmail.com>
2024-04-15 22:16:23 -0500

    Add depth-first search functions
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- edgegraph/traversal/depthfirst.py
+++ edgegraph/traversal/depthfirst.py
@@ -41,6 +41,49 @@ def dft_recursive(uni: Universe,
     visited = {}
     return _dft_recur(uni, start, visited)
 
+def _dfs_recur(uni: Universe,
+               v: Vertex,
+               visited: dict[Vertex, None],
+               attrib: str,
+               val: object) -> Vertex:
+    visited[v] = None
+    for w in helpers.neighbors(v):
+        if (uni is not None) and (w not in uni.vertices):
+            continue
+        if w not in visited:
+            # check for a match first -- then we can exit early
+            if hasattr(w, attrib):
+                print(f"checking {w.i} for {attrib}={val}...", end='')
+                if w[attrib] == val:
+                    print("HIT!")
+                    return w
+                print("nope")
+            ret = _dfs_recur(uni, w, visited, attrib, val)
+            if ret:
+                return ret
+    return None
+
+def dfs_recursive(uni: Universe,
+                  start: Vertex,
+                  attrib: str,
+                  val: object) -> Vertex:
+    """
+    Perform a recursive depth-first search in the given graph for a given
+    attribute.
+
+    .. todo::
+      
+       document this
+    """
+    if len(uni.vertices) == 0:
+        # empty!
+        return None
+    if (uni is not None) and (start not in uni.vertices):
+        raise ValueError("Start vertex not in specified universe!")
+
+    visited = {}
+    return _dfs_recur(uni, start, visited, attrib, val)
+
 def dft_iterative(uni: Universe,
                   start: Vertex) -> list[Vertex]:
     """
@@ -68,4 +111,44 @@ def dft_iterative(uni: Universe,
             for w in helpers.neighbors(v):
                 stack.append(w)
     return discovered
+
+def dfs_iterative(uni: Universe,
+                  start: Vertex,
+                  attrib: str,
+                  val: object) -> Vertex:
+    """
+    Perform a non-recursive depth-first search in the given universe.
+
+    .. todo::
+
+       document this
+
+    .. danger::
+
+       I'm fairly certain this isn't working right just yet.  It searches, but
+       re-visits vertices.
+    """
+    if len(uni.vertices) == 0:
+        # empty!
+        return None
+    if (uni is not None) and (start not in uni.vertices):
+        raise ValueError("Start vertex not in specified universe!")
+
+    stack = [start]
+    discovered = []
+    while len(stack) != 0:
+        v = stack.pop()
+        if hasattr(v, attrib):
+            print(f"checking {v[attrib]} for {attrib}={val}...", end='')
+            if v[attrib] == val:
+                print("HIT!")
+                return v
+            print("nope")
+        if v not in discovered:
+            if (uni is not None) and (v not in uni.vertices):
+                continue
+            discovered.append(v)
+            for w in helpers.neighbors(v):
+                stack.append(w)
+    return None
 
