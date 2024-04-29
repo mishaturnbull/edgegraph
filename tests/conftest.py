a3f1ecb511ff98f314be106f9abebe486b41cbbe
Misha Turnbull <mishaturnbull@gmail.com>
2024-04-28 21:17:25 -0500

    Improve clrs09_22_6 documentation
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,31 +5,51 @@
 Provides fixtures and PyTest hooks for all testing usage.
 
 See:
-https://docs.pytest.org/en/7.1.x/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
+https://docs.pytest.org/en/latest/reference/fixtures.html#conftest-py-sharing-fixtures-across-multiple-files
 """
 
 import pytest
 
-from edgegraph.structure import Vertex, DirectedEdge
+from edgegraph.structure import Universe, Vertex, DirectedEdge
 from edgegraph.builder import adjlist
 
 @pytest.fixture
-def graph_clrs09_22_6():
+def graph_clrs09_22_6() -> tuple[Universe, list[Vertex]]:
     """
     The graph generated in this function is taken from [CLRS09]_, figure 22.6.
 
     .. uml::
 
-       object q
-       object r
-       object s
-       object t
-       object u
-       object v
-       object w
-       object x
-       object y
-       object z
+       object q {
+       0
+       }
+       object r {
+       1
+       }
+       object s {
+       2
+       }
+       object t {
+       3
+       }
+       object u {
+       4
+       }
+       object v {
+       5
+       }
+       object w {
+       6
+       }
+       object x {
+       7
+       }
+       object y {
+       8
+       }
+       object z {
+       9
+       }
 
        q --> s
        q --> t
@@ -45,9 +65,14 @@ def graph_clrs09_22_6():
        x --> z
        y --> q
        z --> x
+
+    :return: a two-tuple containing a
+       :py:class:`~edgegraph.structure.universe.Universe` of the graph, and a
+       :py:class:`list` of all :py:class:`~edgegraph.structure.vertex.Vertex`
+       objects.  The order of this list is shown by the numbers in the above
+       diagram; the number of a vertex is its index in the list.
     """
     verts = [Vertex(attributes={'i': i}) for i in range(10)]
-    #0,1, 2, 3, 4, 5, 6, 7, 8, 9
     q, r, s, t, u, v, w, x, y, z = verts
     adj = {
             q: [s, t, w],
@@ -63,6 +88,4 @@ def graph_clrs09_22_6():
         }
     uni = adjlist.load_adj_dict(adj, DirectedEdge)
     return uni, verts
-
-
 
