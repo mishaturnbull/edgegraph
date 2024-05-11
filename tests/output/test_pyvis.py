9bbad066851d5a6688dc37b78eac192623f6e09c
Misha Turnbull <mishaturnbull@gmail.com>
2024-05-10 19:02:07 -0500

    Cover pyvis customisability
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- tests/output/test_pyvis.py
+++ tests/output/test_pyvis.py
@@ -16,7 +16,7 @@ def test_pyvis_makes_net_default_args(graph_clrs09_22_6):
     Ensure default-arguments make net call makes a net.
     """
     uni, verts = graph_clrs09_22_6
-    pvn = pyvis.make_pyvis_net(uni, rvfunc=lambda v: str(v.i))
+    pvn = pyvis.make_pyvis_net(uni)
     assert len(verts) == len(pvn.get_nodes()), \
             "Did not get the right number of vertices back!"
 
@@ -54,4 +54,20 @@ def test_pyvis_makes_net(graph_clrs09_22_6):
         v2 = verts[int(title[6])]
 
         assert v2 in helpers.neighbors(v1)
+
+def test_pyvis_customizable(graph_clrs09_22_6):
+    uni, _ = graph_clrs09_22_6
+
+    pvn = pyvis.make_pyvis_net(uni,
+            rvfunc=lambda v: str(v.i),
+            refunc=lambda e: f"{e.v1.i} --> {e.v2.i}")
+
+    assert not pvn.conf, "Customizability was enabled by default!"
+
+
+    pvn = pyvis.pyvis_render_customizable(uni,
+            rvfunc=lambda v: str(v.i),
+            refunc=lambda e: f"{e.v1.i} --> {e.v2.i}")
+
+    assert pvn.conf, "Customizability was not enabled!"
 
