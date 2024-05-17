42b5f93b115f03372e7e31b33d7935d8d8ce57b4
Misha Turnbull <mishaturnbull@gmail.com>
2024-05-17 16:49:07 -0500

    Add singleton metaclasses
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

---
+++ edgegraph/structure/singleton.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Provides metaclasses to assist creating singletons.
+
+This module contains helper metaclasses to ease creating custom subclasses of
+:py:class:`~edgegraph.structure.vertex.Vertex`.  It provides two helpers, one
+for creating a so-called "true singleton", and one for creating so-called
+"semi-singletons":
+
+* Global singleton: there can be *only one* instance of global singletons.
+* Semi-singletons: there can be multiple instances of semi-singleton classes,
+   but they are designated with some unique primary key.  Only one instance
+   with a given primary key can exist.
+
+.. todo::
+
+   add code examples to this
+"""
+
+from __future__ import annotations
+
+import json
+
+class TrueSingleton(type):
+    """
+    Metaclass for true singletons.
+
+    .. seealso::
+
+       The design of this metaclass is taken directly from this *excellent*
+       StackOverflow answer by user @agf.  Thanks!!
+
+       https://stackoverflow.com/a/6798042
+    """
+
+    __singleton_instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.__singleton_instances:
+            cls.__singleton_instances[cls] = \
+                    super(TrueSingleton, cls).__call__(*args, **kwargs)
+        return cls.__singleton_instances[cls]
+
+
+def semi_singleton_metaclass(hashfunc=None):
+    """
+    Generate and return a metaclass for semi-singletons.
+
+    .. seealso::
+
+       The design of this metaclass is heavily inspiried by this *excellent*
+       StackOverflow answer by user @agf@.  Thanks!!
+
+       https://stackoverflow.com/a/6798042
+
+       The default hash function also uses an approach for dictionary hashing
+       from StackOverflow user Jack O'Connor.  Thanks!!
+
+       https://stackoverflow.com/a/22003440
+    """
+
+    # by default, use a hash function to serialize all arguments
+    if hashfunc is None:
+
+        def hashfunc(t):
+            kwargs = json.dumps(t[1], sort_keys=True)
+            return hash((t[0], kwargs))
+
+    class _SemiSingleton(type):
+
+        __semisingleton_instance_map = {}
+
+        def __call__(cls, *args, **kwargs):
+            key = hashfunc((args, kwargs))
+            if key not in cls.__semisingleton_instance_map:
+                cls.__semisingleton_instance_map[key] = \
+                        super(_SemiSingleton, cls).__call__(*args, **kwargs)
+            return cls.__semisingleton_instance_map[key]
+
+    return _SemiSingleton
+
--- tests/interact.py
+++ tests/interact.py
@@ -18,11 +18,27 @@ import random
 from edgegraph.builder import randgraph, explicit
 from edgegraph.output import plaintext, plantuml as pu, pyvis
 
+from edgegraph.structure import singleton
+
+class A(object):
+    def __init__(self, *args):
+        print("in A.__init__")
+
+class B(metaclass=singleton.TrueSingleton):
+    def __init__(self, *args):
+        print("in B.__init__")
+
+class C(metaclass=singleton.semi_singleton_metaclass()):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        print("in C.__init__")
+
 def main():
     """
     Main routine.
     """
-    graph = randgraph.randgraph(count=100)
+    graph = randgraph.randgraph(count=15)
     n1 = random.choice(list(graph.vertices))
     n2 = random.choice(list(graph.vertices))
     explicit.link_undirected(n1, n2)
@@ -31,8 +47,6 @@ def main():
 
     with open('out.puml', 'w', encoding='utf-8') as wfp:
         wfp.write(pu.render_to_plantuml_src(graph, pu.PLANTUML_RENDER_OPTIONS))
-
-    pvn = pyvis.pyvis_render_customizable(graph, rfunc=lambda v: str(v.i))
 
     code.interact(local={**locals(), **globals()})
 
