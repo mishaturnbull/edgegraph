b4410a915b1845394c447a05f2c7d679e49428b7
Misha Turnbull <mishaturnbull@gmail.com>
2025-03-06 22:58:01 -0600

    Remove unneeded LBYL check
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- edgegraph/traversal/helpers.py
+++ edgegraph/traversal/helpers.py
@@ -313,8 +313,7 @@ def ineighbors(
                 f"Unknown option for direction_sensitive = {direction_sensitive}"
             )
 
-    if Vertex.NEIGHBOR_CACHING:
-        vert._qa_neighbors_insert(cache, direction_sensitive, unknown_handling, filterfunc)
+    vert._qa_neighbors_insert(cache, direction_sensitive, unknown_handling, filterfunc)
 
 def neighbors(
     vert: Vertex,
