b5c9703e71b44da5964b03e3fb9834b3e0c26a9d
Misha Turnbull <mishaturnbull@gmail.com>
2025-10-18 06:17:48 -0500

    Expose version constants at module level
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- edgegraph/version.py
+++ edgegraph/version.py
@@ -6,6 +6,10 @@ Define version information for edgegraph.
 This is the ONLY place version information should be updated!!
 """
 
+# Take a snapshot of the directory before, to be used for determining what
+# names are added
+_dir_before = set(dir())
+
 #: major version number (the X in vX.Y.Z)
 #:
 #: :type: int
@@ -22,3 +26,8 @@ VERSION_MINOR = 11
 VERSION_PATCH = 0
 
 __version__ = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
+
+# Figure out what variables have been added, and export them to __all__
+_dir_after = set(dir())
+_all = _dir_after - _dir_before - {"_dir_before", "_dir_after"}
+__all__ = list(_all)
