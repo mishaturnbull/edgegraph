1fbf396536841c88add2d58a4adb967907f3a15f
Misha Turnbull <mishaturnbull@gmail.com>
2026-06-22 20:12:17 -0500

    Ruff check & lint
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,14 +20,20 @@ from .fixtures import *  # noqa: F403
 
 LOG = logging.getLogger(__name__)
 
+
 # monkey-patch Vertex to pretty-repr itself with the `i` attribute if
 # available, since so many unit tests use this pattern.  Makes for MUUUUCH
 # easier debugging; but I don't want to make this a global API feature of
 # Vertex -- so for the unittests only!
 def new_repr(inst):
-    if hasattr(inst, 'i') and isinstance(inst.i, int):
+    """
+    Monkey-patched __repr__ for Vertex to print `i` attribute.
+    """
+    if hasattr(inst, "i") and isinstance(inst.i, int):
         return f"<Vertex i={inst.i} @ {hex(id(inst))}>"
     return super(Vertex, inst).__repr__()
+
+
 Vertex.__repr__ = new_repr
 
 
